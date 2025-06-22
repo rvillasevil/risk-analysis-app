ec95f55289e73caf2fa47bfb3f327be71c43142d
rvillasevil <rvillasevil@gmail.com>
2025-06-22 13:02:46 +0200

    resolve mensajes creation

--- test/services/semantic_guard_test.rb
+++ test/services/semantic_guard_test.rb
@@ -25,4 +25,32 @@ class SemanticGuardTest < ActiveSupport::TestCase
     assert_equal SemanticGuard::ENDPOINT, sent[:url]
     assert_equal 0.3, sent[:body][:temperature]
   end
+
+  test 'accepts \"Ok\" as valid response' do
+    risk_assistant = OpenStruct.new(messages: DummyRelation.new)
+    HTTP.stub :headers, ->(*args) {
+      Class.new do
+        define_method(:post) do |url, json:|
+          Struct.new(:parse).new({ 'choices' => [{ 'message' => { 'content' => 'Ok' } }] })
+        end
+      end.new
+    } do
+      result = SemanticGuard.validate(question: 'q', answer: 'a', context: 'c', risk_assistant: risk_assistant, thread_id: 1)
+      assert_nil result
+    end
+  end
+
+  test 'accepts \"OK.\" as valid response' do
+    risk_assistant = OpenStruct.new(messages: DummyRelation.new)
+    HTTP.stub :headers, ->(*args) {
+      Class.new do
+        define_method(:post) do |url, json:|
+          Struct.new(:parse).new({ 'choices' => [{ 'message' => { 'content' => 'OK.' } }] })
+        end
+      end.new
+    } do
+      result = SemanticGuard.validate(question: 'q', answer: 'a', context: 'c', risk_assistant: risk_assistant, thread_id: 1)
+      assert_nil result
+    end
+  end  
 end
